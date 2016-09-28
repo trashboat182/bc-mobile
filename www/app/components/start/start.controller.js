3a58462ea9ec807e5b220db968a9e52f70f86ed4
patomalo <alpattycruz@gmail.com>
2016-09-27 23:55:38 -0400

    automatic slide added

--- www/app/components/start/start.controller.js
+++ www/app/components/start/start.controller.js
@@ -7,14 +7,15 @@
     .module('bcMobile')
     .controller('StartController',StartController);
 
-  function StartController($ionicSlideBoxDelegate, $state) {
+  function StartController($q, $ionicSlideBoxDelegate, $state) {
     var vm = this;
+    var count = 0;
 
     vm.next = function () {
       var slideIndex = $ionicSlideBoxDelegate.currentIndex();
       $ionicSlideBoxDelegate.next();
       if(slideIndex == 2) {
-        $ionicSlideBoxDelegate.slide(0,2000);
+        $ionicSlideBoxDelegate.slide(0);
       }
     };
 
@@ -27,10 +28,26 @@
      $state.go("app.login");
     };
 
-    function _init() {
+    function slideNext(mil) {
+      var deferred = $q.defer();
       setTimeout(function () {
         vm.next();
-      }, 3000);
+        count += 1;
+        deferred.resolve(count);
+      }, mil);
+      return deferred.promise;
+    }
+    function slideMove() {
+      slideNext(4000).then(function (resolve) {
+        console.log(resolve);
+        if(resolve != 9){slideMove();}
+        else {return;}
+      });
+    }
+
+    function _init() {
+      console.log("Welcome to the app");
+      slideMove();
     }
     _init();
   }
