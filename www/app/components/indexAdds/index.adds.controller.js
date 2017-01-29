c1e0b71e503062780cbe6cb0d072c0c7483d175b
patomalo <alpattycruz@gmail.com>
2017-01-29 09:28:30 -0400

    added menu options and app main view

--- www/app/components/indexAdds/index.adds.controller.js
+++ www/app/components/indexAdds/index.adds.controller.js
@@ -7,9 +7,71 @@
   angular
     .module('bcMobile')
     .controller('IndexAddsController', IndexAddsController);
-  function IndexAddsController($firebaseAuth, $firebaseObject) {
+  function IndexAddsController($firebaseAuth, $firebaseObject, $ionicSideMenuDelegate) {
     var vm = this;
     var ref = firebase.database().ref();
+
+    vm.view = {
+      buttons:  {
+        menuButton: menuButton,
+        openCategory: openCategory
+      },
+      categories: [{
+        name:'Inmuebles',
+        icon: 'icon ion-ios-home',
+        status: false,
+        options: [
+          [{name: 'Casa Venta', icon: 'ion-ios-home'}, {name: 'Casa Alquiler', icon: 'ion-ios-home'}],
+          [{name: 'Casa Anticretico', icon: 'ion-ios-home'}, {name: 'Depto. Venta', icon: 'ion-ios-home-outline'}],
+          [{name: 'Depto. Alquiler', icon: 'ion-ios-home-outline'}, {name: 'Depto. Anticretico', icon: 'ion-ios-home-outline'}]
+        ]
+      }, {
+        name:'Automoviles',
+        icon: 'icon ion-model-s',
+        status: false,
+        options: [
+          [{name: 'Camiones', icon: 'ion-model-s'}, {name: 'Vagonetas', icon: 'ion-android-car'}],
+          [{name: 'Usados', icon: 'ion-ios-cart'}, {name: 'Nuevos', icon: 'ion-ios-cart-outline'}]
+        ]
+      }, {
+        name:'Bolsa de Trabajo',
+        icon: 'icon ion-ios-people',
+        status: false,
+        options: [
+          [{name: 'Tiempo Completo', icon: 'ion-ios-star'}, {name: 'Medio Tiempo', icon: 'ion-ios-star-half'}],
+          [{name: 'Docentes', icon: 'ion-ios-people'}, {name: 'Buen sueldo', icon: 'ion-social-usd'}]
+        ]
+      }, {
+        name:'Variedades',
+        icon: 'icon ion-bag',
+        status: false,
+        options: [
+          [{name: 'Ofrece servicios', icon: 'ion-ios-chatboxes'}, {name: 'Cursos', icon: 'ion-ios-bookmarks'}],
+          [{name: 'Bienes', icon: 'ion-social-usd'}, {name: 'Intimidades', icon: 'ion-ios-heart'}]
+        ]
+      }],
+      variable: {}
+    };
+
+    function init() {
+      console.log("Starting anuncio");
+    }
+
+    function menuButton() {
+      console.log("cosas de la vida");
+      $ionicSideMenuDelegate.toggleLeft();
+    }
+
+    function openCategory(index) {
+      for(var key = 0; key < vm.view.categories.length; key++) {
+        var cat = vm.view.categories[key];
+        if(index === key && !cat.status) {
+          cat.status = true;
+        } else {
+          cat.status = false;
+        }
+      }
+    }
 
     /**
      * CREATE A USER
@@ -20,5 +82,7 @@
     }).catch(function(error) {
       console.log("error!!. ", error);
     });*/
+
+    init();
   }
 })();
