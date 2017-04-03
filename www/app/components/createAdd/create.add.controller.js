86ba685f29e1fe0f6c48e1d4e70a82b6b7136199
patomalo <alpattycruz@gmail.com>
2017-04-03 01:42:55 -0400

    Add create anuncio

--- www/app/components/createAdd/create.add.controller.js
+++ www/app/components/createAdd/create.add.controller.js
@@ -8,14 +8,50 @@
     .module('bcMobile')
     .controller('CreateAddController', CreateAddController);
 
-  function CreateAddController() {
+  function CreateAddController($ionicSideMenuDelegate) {
     var vm = this;
     vm.buttons = {
-      menuButton: menuButton
+      menuButton: menuButton,
+      createAnuncio: createAnuncio
     };
+    vm.view = {
+      category: null,
+      subCategory: null
+    };
+    vm.categoryList = [];
+
+    function init() {
+      firebase.database().ref('categrias').on('value', function (response) {
+        console.log("patomalo ---s",response.val());
+        var category = response.val();
+        for(var cat in category) {
+          var list = {
+            category: cat,
+            subCategory: category[cat]
+          };
+          vm.categoryList.push(list);
+        }
+        console.log("patomalo ---t", vm.categoryList);
+      });
+    }
 
     function menuButton() {
       $ionicSideMenuDelegate.toggleLeft();
     }
+
+    function createAnuncio() {
+      console.log("to send", vm.view);
+      var viewKey = firebase.database().ref().child('adds').push().key;
+      firebase.database().ref('adds/' + viewKey).set(vm.view).then(function (response) {
+        console.log("done", response);
+        $ionicPopup.alert({
+          title: 'Anuncio creado'
+        }).then(function () {
+          //$state.go("app.login");
+        });
+      });
+    }
+
+    init();
   }
 })();
